7f5974a2ceda31037408b7f32aa5fcf1bccab4c7
Axel Collard Bovy <acollard@MacBook-Air-de-cespi.local>
2016-12-30 15:26:24 -0300

    Spring boot aplication with angular 2 frontend working

--- compileWeb.sh
+++ compileWeb.sh
@@ -4,5 +4,6 @@ cd FrontendWeb
 echo "Building Angular2 App"
 ng build
 echo "Copying Web output in project's web folder"
-cp -r dist/* ../Backend/web
+rm -rf ../Backend/src/main/resources/static/*
+cp -r dist/* ../Backend/src/main/resources/static/
 cd ..
