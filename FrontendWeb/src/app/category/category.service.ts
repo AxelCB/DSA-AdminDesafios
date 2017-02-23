4cea325585de70007427785c4ed96788ce84dc23
Axel Collard Bovy <axcbovy@gmail.com>
2017-02-23 19:14:27 -0300

    Loading categories from backend server

--- FrontendWeb/src/app/category/category.service.ts
+++ FrontendWeb/src/app/category/category.service.ts
@@ -4,11 +4,23 @@
 
 import { Injectable } from '@angular/core';
 import {Category} from "./category";
-import {CATEGORIES} from "./mock-categories";
+import {Http} from "@angular/http";
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class CategoryService {
+
+  constructor(private http: Http) { }
+
   getCategories(): Promise<Category[]> {
-      return Promise.resolve(CATEGORIES);
+      return this.http.get("http://localhost:8080/api/categories")
+                  .toPromise()
+                  .then(response => response.json().data as Category[])
+                  .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
   }
 }
