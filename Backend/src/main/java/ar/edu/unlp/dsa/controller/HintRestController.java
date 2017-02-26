586e4e1aeb2b6bdc56d7076f85f20647664ad727
Josefina Arcidiacono <josefine2_94@hotmail.com>
2017-02-26 03:02:06 -0300

    Enabling CORS for Frontend endpoints. Fixes #3. port/url parametrization must be improved.

--- Backend/src/main/java/ar/edu/unlp/dsa/controller/HintRestController.java
+++ Backend/src/main/java/ar/edu/unlp/dsa/controller/HintRestController.java
@@ -4,11 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import ar.edu.unlp.dsa.Application;
@@ -30,6 +26,7 @@ public class HintRestController {
 		this.hintRepository = hintRepository;
 	}
 
+	@CrossOrigin(origins = "http://localhost:"+Application.FRONTEND_PORT)
 	@RequestMapping(value = "/{hintId}", method = RequestMethod.GET)
     public Hint getHint(@PathVariable Long hintId) {
 		Hint hint = this.getHintRepository().findOne(hintId);
@@ -40,6 +37,7 @@ public class HintRestController {
     }
 
 	// TODO throws DataIntegrityViolationException
+	@CrossOrigin(origins = "http://localhost:"+Application.FRONTEND_PORT)
 	@RequestMapping(value = "/{hintId}", method = RequestMethod.DELETE)
 	public ResponseEntity<?> deleteHint(@PathVariable Long hintId) {
 		Hint hint = this.getHintRepository().findOne(hintId);
@@ -52,6 +50,7 @@ public class HintRestController {
 		return new ResponseEntity<>(null, httpHeaders, HttpStatus.NO_CONTENT);
 	}
 
+	@CrossOrigin(origins = "http://localhost:"+Application.FRONTEND_PORT)
 	@RequestMapping(value = "/{hintId}", method = RequestMethod.PUT)
 	public ResponseEntity<?> updateHint(@RequestBody Hint input, @PathVariable Long hintId) {
 		Hint hint = this.getHintRepository().findOne(hintId);
@@ -63,6 +62,7 @@ public class HintRestController {
 		return new ResponseEntity<>(null, null, HttpStatus.NO_CONTENT);
 	}
 
+	@CrossOrigin(origins = "http://localhost:"+Application.FRONTEND_PORT)
 	@RequestMapping(method = RequestMethod.POST)
 	ResponseEntity<?> add(@RequestBody Hint input) {
 		Hint result = this.getHintRepository().save(new Hint(input.getDescription(), input.getPointsPercentageCost()));
