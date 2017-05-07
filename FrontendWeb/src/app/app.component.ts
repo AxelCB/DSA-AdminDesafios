9199896654980ec3aaf74d0512ab21505a6d458f
Axel Collard Bovy <axcbovy@gmail.com>
2017-05-07 16:59:41 -0300

    Added logout button in menu, and shows menu options only if logged in. Otherwise, redirects to login.

--- FrontendWeb/src/app/app.component.ts
+++ FrontendWeb/src/app/app.component.ts
@@ -1,6 +1,8 @@
-import {Component, ViewChild} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {MessagesService} from './alert-messages/alert-messages.service';
 import {AlertMessagesComponent} from './alert-messages/alert-messages.component';
+import {AuthService} from './auth/auth.service';
+import {Router} from "@angular/router";
 
 @Component({
   moduleId: module.id,
@@ -8,18 +10,36 @@ import {AlertMessagesComponent} from './alert-messages/alert-messages.component'
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Administración de Desafíos';
+  loggedIn = false;
 
   @ViewChild(AlertMessagesComponent)
   private alertMessagesComponent: AlertMessagesComponent;
 
-  constructor(private messagesService: MessagesService) {
+  constructor(private messagesService: MessagesService, private authService: AuthService, private router: Router) {
     messagesService.missionReceived$.subscribe(message => {
       this.alertMessagesComponent.updateMessage(message);
     });
     messagesService.missionReceivedToAppend$.subscribe(messageToAppend => {
       this.alertMessagesComponent.appendToMessage(messageToAppend);
     });
+    this.authService.loggedInStatusChanged$.subscribe(loggedInStatus => {
+      if (loggedInStatus) {
+        this.loggedIn = true;
+        this.router.navigate(['/']);
+      } else {
+        this.loggedIn = false;
+        this.router.navigate(['/login']);
+      }
+    });
+  }
+
+  ngOnInit() {
+    this.authService.validateSession().subscribe(status => this.loggedIn = status || false, () => {});
+  }
+
+  logout() {
+    this.authService.logout().subscribe(() => this.authService.updateLoggedInStatus(false));
   }
 }
