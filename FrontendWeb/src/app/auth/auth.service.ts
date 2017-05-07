c06e1e286701247b65e4aa0f2d857edb163b4313
Axel Collard Bovy <axcbovy@gmail.com>
2017-05-07 16:57:29 -0300

    Added AuthService to handle authentication services with backend, and easy token retrieval function

--- FrontendWeb/src/app/auth/auth.service.ts
+++ FrontendWeb/src/app/auth/auth.service.ts
@@ -4,13 +4,25 @@ import {Observable} from 'rxjs/Observable';
 import {environment} from '../../environments/environment';
 import {MessagesService} from '../alert-messages/alert-messages.service';
 import {Message} from '../alert-messages/message';
-import {INTERNAL_SERVER_ERROR, NOT_FOUND} from 'http-status-codes';
+import {FORBIDDEN, INTERNAL_SERVER_ERROR, NOT_FOUND, UNAUTHORIZED} from 'http-status-codes';
 import { Http, Headers, RequestOptions } from '@angular/http';
+import {Router} from '@angular/router';
+import {Subject} from 'rxjs/Subject';
 
 @Injectable()
 export class AuthService {
 
-  constructor(private http: Http, private messagesService: MessagesService) {
+  // Observable boolean sources
+  private loggedInStatusChangedSource = new Subject<boolean>();
+
+  // Observable boolean streams
+  loggedInStatusChanged$ = this.loggedInStatusChangedSource.asObservable();
+
+  constructor(private http: Http, private messagesService: MessagesService, private router: Router) {
+  }
+
+  updateLoggedInStatus(newStatus: boolean) {
+    this.loggedInStatusChangedSource.next(newStatus);
   }
 
   login(user: User): Observable<boolean> {
@@ -31,24 +43,54 @@ export class AuthService {
       .catch((error) => this.handleError(error, this.messagesService));
   }
 
-  logout(): Observable<boolean> {
-    let headers = new Headers({ 'Content-Type': 'application/json' });
+  validateSession(): Observable<boolean> {
+    let headers = new Headers({ 'Authorization': this.getToken() });
     let options = new RequestOptions({ headers: headers });
-    return this.http.get(environment.backendUrl  + '/auth/logout', options)
+    return this.http.get(environment.backendUrl  + '/auth/validate', options)
       .map(() => {
-        localStorage.removeItem('loggedUser');
         return true;
       })
       .catch((error) => this.handleError(error, this.messagesService));
   }
 
+  logout(): Observable<boolean> {
+    let headers = new Headers({ 'Authorization': this.getToken() });
+    let options = new RequestOptions({ headers: headers });
+    return this.http.get(environment.backendUrl  + '/auth/logout', options)
+      .map(() => {
+        localStorage.removeItem('loggedUser');
+        this.router.navigate(['/login']);
+        return true;
+      })
+      .catch((error) => this.handleError(error, this.messagesService));
+  }
+
+  getToken(): string {
+    let loggedUser = <User>JSON.parse(localStorage.getItem('loggedUser'));
+    if (loggedUser != null) {
+      return loggedUser.token;
+    } else {
+      return null;
+    }
+  }
+
   private handleError (error: Response | any, messagesService: MessagesService) {
     let errorMessage = new Message();
     errorMessage.isError = true;
-    if (error instanceof Response) {
+    if ((<Response>error).status != null) {
       let responseError = <Response>error;
       errorMessage.responseCode = responseError.status;
       switch (responseError.status) {
+        case UNAUTHORIZED:
+        case FORBIDDEN:
+        case NOT_FOUND:
+          if (this.getToken() != null) {
+            this.logout().subscribe();
+          } else {
+            localStorage.removeItem('loggedUser');
+            this.router.navigate(['/login']);
+          }
+          break;
         case INTERNAL_SERVER_ERROR: {
           errorMessage.content = 'Ha ocurrido un error inesperado. Intente nuevamente más tarde, o vuelva al inicio';
           break;
